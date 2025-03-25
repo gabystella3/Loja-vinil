878c0e8f9499437ed5daf0b533a67a263492fed6
gabystella3 <162180367+gabystella3@users.noreply.github.com>
2025-03-25 19:16:52 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,52 @@
+let dados = [
+    {
+        nome: "Michael Jackson - Thriller",
+        descricao: "Lançado em 30 de novembro de 1982, 'Thriller' é o álbum mais vendido da história, com vendas estimadas entre 51 e 65 milhões de cópias. O disco inclui sucessos como 'Billie Jean', 'Beat It' e 'Thriller', e foi inovador ao utilizar videoclipes como ferramentas de promoção musical.",
+        link: "https://www.amazon.com.br/Michael-Jackson-Thriller-Vinyl/dp/B0CN1QGQCF"
+    },
+    {
+        nome: "Chico Buarque - Construção",
+        descricao: "Lançado em 1971, 'Construção' é um dos álbuns mais aclamados de Chico Buarque, destacando-se pela faixa-título 'Construção', conhecida por sua letra inovadora e crítica social, além de outras canções marcantes como 'Deus Lhe Pague' e 'Cotidiano'.",
+        link: "https://www.amazon.com.br/Constru%C3%A7%C3%A3o-S%C3%A9rie-Cl%C3%A1ssicos-Vinil-Disco/dp/B07DVM698H"
+    },
+    {
+        nome: "Cyndi Lauper - She's So Unusual",
+        descricao: "Álbum de estreia de Cyndi Lauper, lançado em 1983, que trouxe sucessos como 'Girls Just Want to Have Fun', 'Time After Time' e 'She Bop'. O disco foi aclamado por sua mistura de pop e new wave, consolidando Lauper como uma das principais artistas da década de 1980.",
+        link: "https://www.amazon.com.br/Shes-So-Unusual-Disco-Vinil/dp/B07TKNFVQX"
+    },
+    {
+        nome: "Raça Negra - Raça Negra (1991)",
+        descricao: "O álbum homônimo de 1991 do grupo Raça Negra ajudou a popularizar o samba e o pagode no Brasil. Inclui hits como 'Cheia de Manias', 'Doce Paixão' e 'Cigana', que se tornaram clássicos do gênero.",
+        link: "https://www.amazon.com.br/RA%C3%87A-NEGRA-1992-LP-BANDA/dp/B0853DXY21"
+    },
+    {
+        nome: "Banda Calypso - Volume 1",
+        descricao: "Lançado em 1999, 'Volume 1' é o álbum de estreia da Banda Calypso, que introduziu o ritmo calypso ao cenário musical brasileiro. Destaca-se pela faixa 'Vendaval' e marcou o início do sucesso nacional da banda.",
+        link: "https://www.amazon.com.br/Banda-Calypso-1/dp/B00018Q938"
+    },
+    {
+        nome: "Legião Urbana - Dois",
+        descricao: "Lançado em 1986, 'Dois' é o segundo álbum da banda Legião Urbana e é considerado um dos mais importantes do rock brasileiro. Inclui clássicos como 'Eduardo e Mônica', 'Tempo Perdido' e 'Quase Sem Querer'.",
+        link: "https://www.searomdiscos.com.br/products/lp-vinil-legiao-urbana-1985-7d643fa5-bbfc-42fc-a17b-c2f111509c92"
+    },
+    {
+        nome: "Bryan Adams - Reckless",
+        descricao: "Lançado em 1984, 'Reckless' é um dos álbuns mais bem-sucedidos de Bryan Adams, incluindo sucessos como 'Heaven', 'Summer of '69' e 'Run to You'. O disco consolidou Adams como uma estrela do rock nos anos 1980.",
+        link: "https://www.amazon.com.br/Reckless-Disco-Vinil-Bryan-Adams/dp/B00NG63A3O"
+    },
+    {
+        nome: "Zé Ramalho - Zé Ramalho",
+        descricao: "O álbum de estreia homônimo de Zé Ramalho, lançado em 1978, mescla rock com música nordestina e inclui a icônica faixa 'Avôhai', além de outras como 'Chão de Giz' e 'Vila do Sossego'.",
+        link: "https://www.amazon.com.br/LP-RAMALHO-1978-NACIONAL-ORIGINAL/dp/B0DHLH15RG"
+    },
+    {
+        nome: "Reginaldo Rossi - Cheio de Amor",
+        descricao: "Lançado em 1981, 'Cheio de Amor' é um dos álbuns de maior sucesso de Reginaldo Rossi, conhecido como o 'Rei do Brega'. Inclui a faixa 'Em Plena Lua de Mel', um de seus maiores sucessos.",
+        link: "https://www.amazon.com.br/NACIONAL-CHEIO-AMOR-REGINALDO-ROSSI/dp/B09Z8NT4RL"
+    },
+    {
+        nome: "Cazuza - Ideologia (LP Vermelho)",
+        descricao: "Lançado em 1988, 'Ideologia' é um dos álbuns mais emblemáticos de Cazuza, apresentando sucessos como 'Ideologia', 'Brasil' e 'Faz Parte do Meu Show'. O disco reflete a inquietação e o talento poético do artista.",
+        link: "https://www.umusicstore.com/vinil-cazuza-ideologia-lp-vermelho-4685/p?idsku=5727"
+    }
+];
